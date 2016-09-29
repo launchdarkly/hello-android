6946252218c333df168e45f1d3db32411db401b9
Dan Richelson <dan@launchdarkly.com>
2016-09-29 15:37:27 -0700

    Create Example app.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# hello-android
-Example Android app using LaunchDarkly
+### LaunchDarkly Sample Android Application  ###
+We've built a simple application that demonstrates how LaunchDarkly's SDK works.  Below, you'll find the basic build procedure, but for more comprehensive instructions, you can visit your [Quickstart page](https://app.launchdarkly.com/quickstart#/).
+##### Build instructions  #####
+1. Make sure you have [Android Studio](https://developer.android.com/studio/index.html) installed
+2. Open the ```hello-android``` directory
+3. Copy the mobile key from your account settings page and the feature flag key from your LaunchDarkly dashboard into `MainActivity.java` 
+4. Run your application through the Emulator or on a real device.
---
+++ app/src/main/java/com/launchdarkly/hello_android/MainActivity.java
@@ -0,0 +1,38 @@
+package com.launchdarkly.hello_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.launchdarkly.android.LDClient;
+import com.launchdarkly.android.LDConfig;
+import com.launchdarkly.android.LDUser;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "LDMainActivity";
+    private static final String FLAG_KEY = "myBooleanFlag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LDConfig ldConfig = new LDConfig.Builder()
+                .setMobileKey("MOBILE_KEY")
+                .build();
+
+        LDUser user = new LDUser.Builder("user key")
+                .email("fake@example.com")
+                .build();
+
+        LDClient client = LDClient.init(getApplication(), ldConfig, user, 5);
+
+        if (client.boolVariation(FLAG_KEY, false)) {
+            Log.i(TAG, "Showing feature");
+        } else {
+            Log.i(TAG, "Not Showing feature");
+        }
+
+
+    }
+}
