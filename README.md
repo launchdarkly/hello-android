819d39284177d131b8a57c7e95ba72ceab89d57f
Louis Chan <91093020+louis-launchdarkly@users.noreply.github.com>
2024-04-24 10:47:00 -0700

    Update the Hello App following the latest specifications (#10)
    * fix: Update the Hello App and README following the latest specifications
    
    * fix: Add CODEOWNERS
    
    * fix: Remove alert for flag key given we have a default that will be created by the Quickstart guide

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # LaunchDarkly sample Android application
 
 We've built a simple mobile application written in Kotlin that demonstrates how LaunchDarkly's SDK works.
-Below, you'll find the basic build procedure, but for more comprehensive instructions, you can visit your [Quickstart page](https://app.launchdarkly.com/quickstart#/) or the [Android reference guide](https://docs.launchdarkly.com/sdk/client-side/android).
+
+Below, you'll find the build procedure. For more comprehensive instructions, you can visit your [Quickstart page](https://app.launchdarkly.com/quickstart#/) or the [Android reference guide](https://docs.launchdarkly.com/sdk/client-side/android).
 
 ## Build instructions
 
 1. Make sure you have [Android Studio](https://developer.android.com/studio/index.html) installed.
 2. Edit `MainApplication.kt` and `MainActivity.kt` in the `src/main/java/com/launchdarkly/hello_android` directory and set the value of LAUNCHDARKLY_MOBILE_KEY to your LaunchDarkly mobile key. If there is an existing boolean feature flag in your LaunchDarkly project that you want to evaluate, set BOOLEAN_FLAG_KEY to the flag key.
 
-```
+```kotlin
     val LAUNCHDARKLY_MOBILE_KEY = "mobile-key-from-launch-darkly-website"
-    
+
     val BOOLEAN_FLAG_KEY = "my-boolean-flag"
 ```
 
 3. Run your application through the Emulator or on a real device.
 
-You should receive the message ”Feature flag ‘<flag key>’ is <true/false> for this context”.
+You should see the message "The <flagKey> feature flag evaluates to <flagValue>.", the application will run continuously and react to the flag changes in LaunchDarkly.
 
 ## Looking for a Java example?
 
--- app/src/main/java/com/launchdarkly/hello_android/MainActivity.kt
+++ app/src/main/java/com/launchdarkly/hello_android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.launchdarkly.hello_android
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -10,18 +11,13 @@ import com.launchdarkly.sdk.android.LDClient
 class MainActivity : AppCompatActivity() {
 
     // Set BOOLEAN_FLAG_KEY to the feature flag key you want to evaluate.
-    val BOOLEAN_FLAG_KEY = "my-boolean-flag"
+    val BOOLEAN_FLAG_KEY = "sample-feature"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView : TextView = findViewById(R.id.textview)
-
-        if (BOOLEAN_FLAG_KEY == "my-boolean-flag") {
-            val builder = AlertDialog.Builder(this)
-            builder.setMessage("BOOLEAN_FLAG_KEY was not customized for this application.")
-            builder.create().show()
-        }
+        val fullView : View = window.decorView
 
         if (LAUNCHDARKLY_MOBILE_KEY == "mobile-key-from-launch-darkly-website") {
             val builder = AlertDialog.Builder(this)
@@ -30,26 +26,36 @@ class MainActivity : AppCompatActivity() {
         }
 
         val client = LDClient.get()
+        val flagValue = client.boolVariation(BOOLEAN_FLAG_KEY, false)
 
         // to get the variation the SDK has cached
         textView.text = getString(
             R.string.flag_evaluated,
             BOOLEAN_FLAG_KEY,
-            client.boolVariation(BOOLEAN_FLAG_KEY, false).toString()
+            flagValue.toString()
         )
+
+        // Style the display
+        textView.setTextColor(resources.getColor(R.color.colorText))
+        if(flagValue) {
+            fullView.setBackgroundColor(resources.getColor(R.color.colorBackgroundTrue))
+        } else {
+            fullView.setBackgroundColor(resources.getColor(R.color.colorBackgroundFalse))
+        }
 
         // to register a listener to get updates in real time
         client.registerFeatureFlagListener(BOOLEAN_FLAG_KEY) {
+            val changedFlagValue = client.boolVariation(BOOLEAN_FLAG_KEY, false)
             textView.text = getString(
                 R.string.flag_evaluated,
                 BOOLEAN_FLAG_KEY,
-                client.boolVariation(BOOLEAN_FLAG_KEY, false).toString()
+                changedFlagValue.toString()
             )
+            if(changedFlagValue) {
+                fullView.setBackgroundColor(resources.getColor(R.color.colorBackgroundTrue))
+            } else {
+                fullView.setBackgroundColor(resources.getColor(R.color.colorBackgroundFalse))
+            }
         }
-
-        // This call is just to make sure all evaluation events show up immediately for this demo.
-        // Otherwise they will be sent at some point in the future.  You don't need to call this
-        // in production, it is handled automatically at a interval (it is customizable).
-        client.flush()
     }
 }
